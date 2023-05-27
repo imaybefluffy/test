55611fc816c015403f237acf2e342ad63c841811
unknown <inkfluffychocolate@gmail.com>
2023-05-27 16:22:41 -0400

    js compression

--- script/main.js
+++ script/main.js
@@ -4,22 +4,14 @@ var contactIcon = document.getElementById("contact-icon");
 
 function contact() {
   if (header.classList.contains("contact-info-menu")) {
-    contactDisappear();
+    header.classList.remove("contact-info-menu");
+    contactButton.classList.remove("borderless");
+    contactIcon.classList.add("fa-address-card-o");
+    contactIcon.classList.remove("fa-close");
   } else {
-    contactAppear();
+    header.classList.add("contact-info-menu");
+    contactButton.classList.add("borderless");
+    contactIcon.classList.remove("fa-address-card-o");
+    contactIcon.classList.add("fa-close");
   }
-}
-
-function contactDisappear() {
-  header.classList.remove("contact-info-menu");
-  contactButton.classList.remove("borderless");
-  contactIcon.classList.add("fa-address-card-o");
-  contactIcon.classList.remove("fa-close");
-}
-
-function contactAppear() {
-  header.classList.add("contact-info-menu");
-  contactButton.classList.add("borderless");
-  contactIcon.classList.remove("fa-address-card-o");
-  contactIcon.classList.add("fa-close");
 }
